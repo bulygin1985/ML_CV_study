5ec132c57804d12d42fc3eff7dc17c2103468a7f
Odysseus <a.andrusyk@steel.mil.ua>
2024-11-13 16:26:27 +0200

    Improvement of svm_loss_vectorized

--- a.andrusyk/assignments/assignment1_colab/assignment1/cs231n/classifiers/linear_svm.py
+++ a.andrusyk/assignments/assignment1_colab/assignment1/cs231n/classifiers/linear_svm.py
@@ -94,7 +94,9 @@ def svm_loss_vectorized(W, X, y, reg):
     # note: it is correct and fast
     scores_yi = scores[range(num_train), y].reshape(-1, 1)
 
-    loss = np.sum(np.maximum(np.zeros((num_train, num_classes)), scores - scores_yi + 1.0))/num_train - 1.0
+    boundaries = scores - scores_yi + 1.0
+
+    loss = np.sum(np.maximum(np.zeros((num_train, num_classes)), boundaries))/num_train - 1.0
     + reg * np.sum(W * W)
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
@@ -110,7 +112,6 @@ def svm_loss_vectorized(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    boundaries = scores - scores_yi + 1.0
     boundaries[boundaries < 0] = 0
     boundaries[range(num_train), y] = 0
     boundaries[boundaries > 0] = 1
